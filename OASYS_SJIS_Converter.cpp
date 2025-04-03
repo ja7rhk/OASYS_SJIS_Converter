b35e30d970ccdef0a7e542dd7bc2e26215fbf491
Sumio Koseki <koseki.sumio@nifty.com>
2025-04-03 14:49:30 +0900

    bug fix

--- OASYS_SJIS_Converter.cpp
+++ OASYS_SJIS_Converter.cpp
@@ -9,7 +9,7 @@
 #include <sstream>
 #include <fstream>
 
-//---- JIS文字をSJIS文字に変換する関数
+// JISコードからSJISコードに変換
 wchar_t jis2sjis(wchar_t jis) {
 
 	wchar_t c1 = (jis >> 8) & 0xFF;
@@ -32,6 +32,32 @@ wchar_t jis2sjis(wchar_t jis) {
 		c1 = (c1 + 0x161) >> 1;
 
 	return c1 << 8 | c2;
+}
+
+// ASCIIコード範囲内かチェック
+bool is_ascii(char ascii) {
+
+	if (ascii >= 0x20 && ascii < 0x7F)
+		return true;
+
+	return false;
+}
+
+// JISコード範囲内かチェック
+bool is_jis(wchar_t jis) {
+
+	wchar_t jis_high = (jis >> 8) & 0xFF;
+	wchar_t jis_low  = jis & 0xFF;
+
+	// 全角英数、かな
+	if ((jis_high >= 0x21 && jis_high <= 0x27) && (jis_low >= 0x21 && jis_low <= 0x7E))
+		return true;
+
+	// 漢字コード
+	if ((jis_high >= 0x30 && jis_high <= 0x74) && (jis_low >= 0x21 && jis_low <= 0x7E))
+		return true;
+
+	return false;
 }
 
 int main(int argc, char* argv[])
@@ -100,15 +126,18 @@ int main(int argc, char* argv[])
 
 		wchar_t jis = (jis_high << 8) + jis_low;
 
-		// NULLコードを検出した場合の処理
+		// NULLコードを検出した場合の処理 (改行)
 		if (jis_high == 0)
 		{
-			if ((jis_low < 0x20) || (jis_low > 0x74))
+			// 下線付きの場合はbit7が1になっているので両方チェックする
+			char low_byte = jis_low & 0x7F;
+
+			if ((low_byte < 0x20) || (low_byte > 0x74))
 			{
 				// 次のバイトがASCIIまたはJISコード以外は無視する
 				continue;
 			}
-			else if ((jis_low >= 0x20) && (jis_low <= 0x74))
+			else if ((low_byte >= 0x20) && (low_byte <= 0x74))
 			{
 				// 次のバイトがASCIIまたはJISコードの場合はLFを挿入する
 				jis_low = 0x0A;
@@ -118,14 +147,18 @@ int main(int argc, char* argv[])
 			}
 		}
 
-		// ASCIIコード (2バイト目に0x80を足している)
-		if ((jis & 0x0080) != 0 && (jis < 0x7F00))
+		// 半角英数の場合 (ASCIIコード)
+		if ((jis & 0x8080) == 0x0080)
 		{
+			// ASCIIコード (2バイト目に0x80を足している)
 			jis_low &= 0x7F;
-			ofs.write((char*)(&jis_high), sizeof(char));
-			num_byte++;
+			if (is_ascii(jis_low))
+			{
+				ofs.write((char*)(&jis_high), sizeof(char));
+				num_byte++;
+			}
 			// 2バイト目が'_'の場合は半角詰めなので変換しない
-			if (jis_low != 0x5F)
+			if (is_ascii(jis_low) && jis_low != 0x5F)
 			{
 				ofs.write((char*)(&jis_low), sizeof(char));
 				num_byte++;
@@ -133,25 +166,53 @@ int main(int argc, char* argv[])
 			i++;
 			continue;
 		}
-		// 半角カナ ("･･"しか使っていないので、半角カナは全部"･･"にする)
-		if ((jis & 0x8080) != 0 && (jis < 0xC000))
+
+		// 上付き文字の場合 (ASCIIコード)
+		if ((jis & 0x8080) == 0x8080)
 		{
-			sjis = 0xA5A5;	// "･･"
-			w_flag = true;
+			// 半角カナ"･･"の場合はSJISの"･･"に変換する
+			if ((jis == 0x8585))
+			{
+				sjis = 0xA5A5;	// "･･"
+				w_flag = true;
+			}
+			// それ以外は半角英数と同じに扱う
+			jis_high &= 0x7F;
+			jis_low &= 0x7F;
+			if (is_ascii(jis_low))
+			{
+				ofs.write((char*)(&jis_high), sizeof(char));
+				num_byte++;
+			}
+			// 2バイト目が'_'の場合は半角詰めなので変換しない
+			if (is_ascii(jis_low) && jis_low != 0x5F)
+			{
+				ofs.write((char*)(&jis_low), sizeof(char));
+				num_byte++;
+			}
+			i++;
+			continue;
 		}
 
 		// 全角英数、かな
-		if ((jis_high >= 0x21 && jis_high <= 0x27) && (jis_low >= 0x21 && jis_low <= 0x7E))
+		if ((jis & 0x8080) == 0)
 		{
-			sjis = jis2sjis(jis);
-			w_flag = true;
+			if (is_jis(jis))
+			{
+				sjis = jis2sjis(jis);
+				w_flag = true;
+			}
 		}
 
-		// 漢字コード
-		if ((jis_high >= 0x30 && jis_high <= 0x74) && (jis_low >= 0x21 && jis_low <= 0x7E))
+		// 下線付きの全角英数、かな
+		if ((jis & 0x8080) == 0x8000)
 		{
-			sjis = jis2sjis(jis);
-			w_flag = true;
+			jis &= 0x7F7F;
+			if (is_jis(jis))
+			{
+				sjis = jis2sjis(jis);
+				w_flag = true;
+			}
 		}
 
 		//JISコードの範囲外
