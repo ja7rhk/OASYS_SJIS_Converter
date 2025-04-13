04e160418cc3ab60346e117b28bb99e037db057c
Sumio Koseki <koseki.sumio@nifty.com>
2025-04-13 15:13:22 +0900

    changed README.md

---
+++ run_oas2text.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+ECHO Running AMeDAS Converter for %USERNAME% on %DATE% %TIME%
+
+
+OASYS_SJIS_Converter.exe 018)イムジャ・ツェ紀行①.oas
+
+OASYS_SJIS_Converter.exe 019)イムジャ・ツェ紀行②.oas
+
+OASYS_SJIS_Converter.exe 020)イムジャ・ツェ紀行③.oas
+
+OASYS_SJIS_Converter.exe 021)イムジャ・ツェ紀行④.oas
+
+:: wait for the user to press a key before closing the window
+pause
